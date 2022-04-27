070a98da03b530af2b3658ac54929ff63774b0d9
Rohan McLure <rmclure@linux.ibm.com>
2022-04-27 10:44:34 +1000

    Update entrypoint to account for new user

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,20 @@ set -e
 if [ "$1" = 'server' ]; then
   shift
   # Figure out public address
+  export GHIDRA_REPOS_DIR=/home/ghidra/repos
   export GHIDRA_PUBLIC_HOSTNAME=${GHIDRA_PUBLIC_HOSTNAME:-$(dig +short myip.opendns.com @resolver1.opendns.com)}
 
   # Add users
   GHIDRA_USERS=${GHIDRA_USERS:-admin}
-  if [ ! -e "/repos/users" ] && [ ! -z "${GHIDRA_USERS}" ]; then
-    mkdir -p /repos/~admin
+  if [ ! -e "$GHIDRA_REPOS_DIR/users" ] && [ ! -z "${GHIDRA_USERS}" ]; then
+    mkdir -p $GHIDRA_REPOS_DIR/~admin
     for user in ${GHIDRA_USERS}; do
       echo "Adding user: ${user}"
-      echo "-add ${user}" >> /repos/~admin/adm.cmd
+      echo "-add ${user}" >> $GHIDRA_REPOS_DIR/~admin/adm.cmd
     done
   fi
 
-  exec "/ghidra/server/ghidraSvr" console
+  exec "/home/ghidra/server/ghidraSvr" console
 fi
 
 exec "$@"
